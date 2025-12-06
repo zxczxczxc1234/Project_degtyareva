c035a2a099d46a23c861920c6805c4eb530df296
bb88 <ghlwhy77@gmail.com>
2025-12-06 17:02:00 +0300

    first commit

--- PZ_3/PZ_3.2.py
+++ PZ_3/PZ_3.2.py
@@ -1,8 +1,15 @@
 # Даны три числа
 #найти среднее из них(то число, расположенное между наибольшим и наименьшим)
-a = int(input("Введите число a: "))
-b = int(input("Введите число b: "))
-c = int(input("Введите число c: "))
-
-middle = a + b + c - min(a, b, c) - max(a, b, c)
-print(f"Среднее число: {middle}")
+try:
+    a = int(input("Введите число a: "))
+    b = int(input("Введите число b: "))
+    c = int(input("Введите число c: "))
+    if (b < a < c) or (c < a < b):
+        middle = a
+    elif (a < b < c) or (c < b < a):
+        middle = b
+    else:
+        middle = c
+    print(f"Среднее число: {middle}")
+except ValueError:
+    print("Неверный ввод.Попробуйте снова.")
